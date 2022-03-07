bbcf8cad618588ce970340896f0f37724422c9a5
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2022-03-07 11:26:19 -0800

    Update fiberStrains.py

--- SectionRegions/fiberStrains.py
+++ SectionRegions/fiberStrains.py
@@ -57,7 +57,7 @@ def parse_args(args) -> dict:
 
     return opts
 
-def getStrains(a, dsr, sec, vminset, vmaxset):
+def getStrains(a, dsr, sec):
     dataDir = os.getcwd()+"\\"+currentDataFolder+"_"+a
     times = np.loadtxt(dataDir+"\\"+os.listdir(dataDir)[0])[:, 0]
     if a == "po":
@@ -69,16 +69,6 @@ def getStrains(a, dsr, sec, vminset, vmaxset):
     files = []
     epsRaw = []
     for ds in dsr:
-        if ds == "dsr6":
-            if vminset is None:
-                vminset = -0.075
-            if vmaxset is None:
-                vmaxset = 0.075
-        else:
-            if vminset is None:
-                vminset = -0.02
-            if vmaxset is None:
-                vmaxset = 0.04
         startSeq = ds + "_" + sec + "_"
         for file in os.listdir(dataDir):
             if file.startswith(startSeq):
@@ -90,12 +80,12 @@ def getStrains(a, dsr, sec, vminset, vmaxset):
                 epsRaw.append(np.loadtxt(dataDir+"\\"+file)[:, 2])
     if len(X) == 0:
         print("no fibers to plot! check DS definition and/or dsr option")
-        return None, None, None, None, None, None, None
+        return None, None, None, None, None
     else:
         eps = np.zeros([len(X), len(epsRaw[0])])
         for i in range(len(X)):
             eps[i, :] = epsRaw[i].T
-        return X, Y, eps, intFrames, vminset, vmaxset, times
+        return X, Y, eps, intFrames, times
 
 def yieldpt(X, Y, eps, times):
     for t in range(eps.shape[1]):
@@ -127,74 +117,93 @@ def yieldpt(X, Y, eps, times):
             plt.show()
             return timeYield, coordsYieldedFibers, epsYieldedFibers
 
-def plasticHingePt(X5, Y5, eps5, times5, X6, Y6, eps6, times6):
+def ultimatePt(X5, Y5, eps5, times5, X6, Y6, eps6, times6):
     for t in range(eps5.shape[1]):
         epst5 = eps5[:,t]
         epst6 = eps6[:,t]
         if any(epst5 <= -0.011)  or  any(epst6 >= 0.09):
-            timeHinge = t
-            print("\nthe plastic hinge point occurs at timepoint ", t, ".")
-            iHingeFibers5 = np.arange(len(X5))[epst5 <= -0.011]
-            XhingeFibers5 = np.array(X5)[iHingeFibers5]
-            YhingeFibers5 = np.array(Y5)[iHingeFibers5]
-            coordsHingeFibers5 = np.column_stack((XhingeFibers5, YhingeFibers5))
-            epsHingeFibers5 = epst5[iHingeFibers5]
-            iHingeFibers6 = np.arange(len(X6))[epst6 >= 0.09]
-            XhingeFibers6 = np.array(X6)[iHingeFibers6]
-            YhingeFibers6 = np.array(Y6)[iHingeFibers6]
-            coordsHingeFibers6 = np.column_stack((XhingeFibers6, YhingeFibers6))
-            epsHingeFibers6 = epst6[iHingeFibers6]
-            hingeSummary5 = pd.DataFrame(np.column_stack( (coordsHingeFibers5, epsHingeFibers5, [t]*len(coordsHingeFibers5), ["concrete"]*len(coordsHingeFibers5)) ), columns = ["Fiber X Coord", "Fiber Y Coord", "Strain", "Timepoint", "Material"])
-            hingeSummary6 = pd.DataFrame(np.column_stack( (coordsHingeFibers6, epsHingeFibers6, [t]*len(coordsHingeFibers6), ["steel"]*len(coordsHingeFibers6)) ), columns = ["Fiber X Coord", "Fiber Y Coord", "Strain", "Timepoint", "Material"])
-            hingeSummary = hingeSummary5.append(hingeSummary6)
-            hingeSummary.to_csv("HingeSummary.csv", index=False)
-            print("the coordinates and corresponding strains of hinged fibers are:")
-            print(hingeSummary)
+            timeUlt = t
+            print("\nthe ultimate point occurs at timepoint ", t, ".")
+            iUltFibers5 = np.arange(len(X5))[epst5 <= -0.011]
+            XUltFibers5 = np.array(X5)[iUltFibers5]
+            YUltFibers5 = np.array(Y5)[iUltFibers5]
+            coordsUltFibers5 = np.column_stack((XUltFibers5, YUltFibers5))
+            epsUltFibers5 = epst5[iUltFibers5]
+            iUltFibers6 = np.arange(len(X6))[epst6 >= 0.09]
+            XUltFibers6 = np.array(X6)[iUltFibers6]
+            YUltFibers6 = np.array(Y6)[iUltFibers6]
+            coordsUltFibers6 = np.column_stack((XUltFibers6, YUltFibers6))
+            epsUltFibers6 = epst6[iUltFibers6]
+            ultSummary5 = pd.DataFrame(np.column_stack( (coordsUltFibers5, epsUltFibers5, [t]*len(coordsUltFibers5), ["concrete"]*len(coordsUltFibers5)) ), columns = ["Fiber X Coord", "Fiber Y Coord", "Strain", "Timepoint", "Material"])
+            ultSummary6 = pd.DataFrame(np.column_stack( (coordsUltFibers6, epsUltFibers6, [t]*len(coordsUltFibers6), ["steel"]*len(coordsUltFibers6)) ), columns = ["Fiber X Coord", "Fiber Y Coord", "Strain", "Timepoint", "Material"])
+            ultSummary = ultSummary5.append(ultSummary6)
+            ultSummary.to_csv("UltimateSummary.csv", index=False)
+            print("the coordinates and corresponding strains of failed fibers are:")
+            print(ultSummary)
             fig = plt.figure(figsize=(6, 5))
             nx = ny = len(X)
             data = eps[:, 0]
             plt.scatter(np.append(X5, X6), np.append(Y5, Y6), c=np.append(eps5[:, timeYield], eps6[:, timeYield]), vmin=-0.003, vmax=0.003)
             plt.colorbar(label="strain")
-            plt.scatter(np.append(XhingeFibers5, XhingeFibers6), np.append(YhingeFibers5, YhingeFibers6), marker='x', color="r", label="Hinged Fibers")
+            plt.scatter(np.append(XUltFibers5, XUltFibers6), np.append(YUltFibers5, YUltFibers6), marker='x', color="r", label="Failed Fibers")
             plt.xlabel("Section Horizontal (X) Axis [inches]")
             plt.ylabel("Section Vertical (Y) Axis [inches]")
-            plt.title("Strains at point of plastic hinging (timepoint "+str(t)+")")
+            plt.title("Strains at point of ultimate curvature (timepoint "+str(t)+")")
             plt.grid()
             plt.xlim([-50, 50])
             plt.ylim([-50, 50])
             plt.legend()
-            plt.gcf().savefig("HingePoint.png")
+            plt.gcf().savefig("UltimatePoint.png")
             plt.show()
-            return timeHinge, list(coordsHingeFibers5).append(list(coordsHingeFibers6)), list(epsHingeFibers5).append(list(epsHingeFibers6))
+            return timeUlt, list(coordsUltFibers5).append(list(coordsUltFibers6)), list(epsUltFibers5).append(list(epsUltFibers6))
 
-def getPushover(a, timeYield, timeHinge):
+def getPushover(a, timeYield, timeUlt):
     if a == "cyclic":
         return None
     else:
         dataDir = os.getcwd()+"\\"+currentDataFolder+"_"+a
-        disp = (np.loadtxt(dataDir+"\\nodeDisp.txt"))[:,0]
-        force = -(np.loadtxt(dataDir+"\\nodeReaction.txt"))[:,0]
         curv = -(np.loadtxt(dataDir+"\\eleDef1.txt"))[:,2]
         mom = -(np.loadtxt(dataDir+"\\eleForce1.txt"))[:,2]
 
-        # Plot moment-curvature and calculate yield, hinge, and capacity displacement
+        # Plot moment-curvature and yield and ultimate points
         fig = plt.figure(figsize=(6, 5))
         plt.plot(np.append([0],curv), np.append([0],mom), zorder=0)
-        curvYield = curv[timeYield] # phi_y (SMALL y =
+        curvYield = curv[timeYield]
         momYield = mom[timeYield]
-        curvHinge = curv[timeHinge]
-        momHinge = mom[timeHinge]
+        curvUlt = curv[timeUlt]
+        momUlt = mom[timeUlt]
         print("\nYield curvature = ", curvYield, " rad/in, Yield moment = ", momYield, " kip-in")
-        print("Hinge curvature = ", curvHinge, " rad/in, Hinge moment = ", momHinge, " kip-in")
-        plt.scatter(curv[timeYield], mom[timeYield], label="Yield Point ("+str(curv[timeYield])+", "+str(mom[timeYield])+")", marker="o", color=[0.0, 0.0, 0.0])
-        plt.scatter(curv[timeHinge], mom[timeHinge], label="Hinge Point ("+str(curv[timeHinge])+", "+str(mom[timeHinge])+")", marker="s", color=[0.0, 0.0, 0.0])
+        print("Ultimate curvature = ", curvUlt, " rad/in, Ultimate moment = ", momUlt, " kip-in")
+        plt.scatter(curvYield, momYield, label="Yield Point ("+str(curvYield)+", "+str(momYield)+")", marker="o", color=[0.0, 0.0, 0.0])
+        plt.scatter(curvUlt, momUlt, label="Ultimate Point ("+str(curvUlt)+", "+str(momUlt)+")", marker="s", color=[0.0, 0.0, 0.0])
         plt.xlabel("Curvature [rad/in]")
         plt.ylabel("Moment [kip-in]")
         plt.title("Pushover Curve - Moment Curvature Analysis")
         plt.legend()
         plt.grid()
         plt.tight_layout()
-        plt.gcf().savefig("PushoverMC.png")
+        plt.gcf().savefig("Pushover.png")
+        plt.show()
+
+def getCyclic(a):
+    if a == "po":
+        return None
+    else:
+        dataDir = os.getcwd()+"\\"+currentDataFolder+"_"+a
+        disp = (np.loadtxt(dataDir+"\\nodeDisp.txt"))[:,0]
+        force = -(np.loadtxt(dataDir+"\\nodeReaction.txt"))[:,0]
+
+        # Plot force-displacement
+        fig = plt.figure(figsize=(6, 5))
+        plt.plot(np.append([0],disp), np.append([0],force), zorder=0)
+        plt.xlabel("Displacement [in]")
+        plt.ylabel("Force [kip]")
+        plt.title("Cyclic Curve")
+        plt.xlim([0, max(disp)+5])
+        plt.ylim([0, max(force)+50])
+        plt.grid()
+        plt.tight_layout()
+        plt.gcf().savefig("Cyclic.png")
         plt.show()
 
 def animate_heat_map(X, Y, eps, intFrames, vminset, vmaxset):
@@ -239,16 +248,31 @@ def animate_heat_map(X, Y, eps, intFrames, vminset, vmaxset):
     plt.show()
 
 if __name__ == "__main__":
+
     opts = parse_args(sys.argv[1:])
-    X, Y, eps, intFrames, vminset, vmaxset, times = getStrains(opts["a"], opts["dsr"], opts["sec"], opts["vminset"], opts["vmaxset"])
+
+    X, Y, eps, intFrames, times = getStrains(opts["a"], opts["dsr"], opts["sec"])
+
+    if opts["a"] == 'po':
+        if opts["vminset"] is None:
+            vminset = -0.02
+        if opts["vmaxset"] is None:
+            vmaxset = 0.04
+    else:
+        if opts["vminset"] is None:
+            vminset = -0.001
+        if opts["vmaxset"] is None:
+            vmaxset = 0.001
     animate_heat_map(X, Y, eps, intFrames, vminset, vmaxset)
 
     if np.isin("dsr6", opts["dsr"]) and np.isin("dsr5", opts["dsr"]) and opts["a"] == 'po':
-        X6, Y6, eps6, intFrames6, vminset6, vmaxset6, times6 = getStrains(opts["a"], ["dsr6"], opts["sec"], opts["vminset"], opts["vmaxset"])
+        X6, Y6, eps6, intFrames6, times6 = getStrains(opts["a"], ["dsr6"], opts["sec"])
         timeYield, coordsYieldedFibers, epsYieldedFibers = yieldpt(X6, Y6, eps6, times6)
 
-        X5, Y5, eps5, intFrames5, vminset5, vmaxset5, times5 = getStrains(opts["a"], ["dsr5"], opts["sec"], opts["vminset"], opts["vmaxset"])
-        timeHinge, coordsHingeFibers, epsHingeFibers = plasticHingePt(X5, Y5, eps5, times5, X6, Y6, eps6, times6)
+        X5, Y5, eps5, intFrames5, times5 = getStrains(opts["a"], ["dsr5"], opts["sec"])
+        timeUlt, coordsUltFibers, epsUltFibers = ultimatePt(X5, Y5, eps5, times5, X6, Y6, eps6, times6)
 
-        getPushover(opts["a"], timeYield, timeHinge)
-    
+        getPushover(opts["a"], timeYield, timeUlt)
+
+    if opts["a"] == 'cyclic':
+        getCyclic(opts["a"])
