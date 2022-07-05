b90a6e4ec7007cce0ad619163544d214d6be75fd
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2022-07-05 14:04:02 -0700

    cc - postprocessing scripts

--- postprocessing/hwdBash.sh
+++ postprocessing/hwdBash.sh
@@ -5,9 +5,9 @@ for model in $(find -maxdepth 1 -name "hwd*.tcl"); do
     opensees.exe $model
     python -m render data$modelName/modelDetails.json data$modelName/modesPostG.yaml -o data$modelName/modes.html -s 200 --vert 3;
     python -m render data$modelName/modelDetails.json data$modelName/dispsGrav.yaml -o data$modelName/dispsGrav.html -s 200 --vert 3;
-    # python Scripts/ModeID.py 
-    python Scripts/plotPds.py data$modelName/ data$modelName/;
-    # python Scripts/plotRH.py senorsRH.out datahwd10.1.$i/RH.out &&
-    # python Scripts/compareRH.py sensorRH.out datahwd10.1.$i/RH.out &&
-    # python Scripts/png2md.py datahwd10.1.$i/plot.png &&
+    # python ../../Scripts/ModeID.py 
+    python ../../Scripts/postprocessing/plotPds.py data$modelName/ data$modelName/;
+    # python ../../Scripts/plotRH.py senorsRH.out datahwd10.1.$i/RH.out &&
+    # python ../../Scripts/compareRH.py sensorRH.out datahwd10.1.$i/RH.out &&
+    # python ../../Scripts/png2md.py datahwd10.1.$i/plot.png &&
     done 
