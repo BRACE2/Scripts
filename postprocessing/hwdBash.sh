cd94fff26879db152547a5ba18bcb4810b2228cd
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2022-07-06 07:25:43 -0700

    cc - update hwdBash.sh and plotPds.py

--- postprocessing/hwdBash.sh
+++ postprocessing/hwdBash.sh
@@ -1,13 +1,27 @@
 cd ../../CalTrans.Hayward/Procedures
+# N=6
+# for model in $(find -maxdepth 1 -name "hwd*.tcl"); do
+#     ((i=i%N)); ((i++==0)) && wait
+#     { modelName=`basename ${model/./} .tcl`
+#     echo "running $modelName"
+#     opensees.exe $model
+#     python -m render data$modelName/modelDetails.json data$modelName/modesPostG.yaml -o data$modelName/modes.html -s 200 --vert 3 &&
+#     python -m render data$modelName/modelDetails.json data$modelName/dispsGrav.yaml -o data$modelName/dispsGrav.html -s 200 --vert 3 &&
+#     # python ../../Scripts/ModeID.py 
+#     python ../../Scripts/postprocessing/plotPds.py data$modelName/ data$modelName/; } &
+#     # python ../../Scripts/plotRH.py senorsRH.out datahwd10.1.$i/RH.out &&
+#     # python ../../Scripts/compareRH.py sensorRH.out datahwd10.1.$i/RH.out &&
+#     # python ../../Scripts/png2md.py datahwd10.1.$i/plot.png &&
+# done
 for model in $(find -maxdepth 1 -name "hwd*.tcl"); do
     modelName=`basename ${model/./} .tcl`
     echo "running $modelName"
     opensees.exe $model
-    python -m render data$modelName/modelDetails.json data$modelName/modesPostG.yaml -o data$modelName/modes.html -s 200 --vert 3;
-    python -m render data$modelName/modelDetails.json data$modelName/dispsGrav.yaml -o data$modelName/dispsGrav.html -s 200 --vert 3;
+    python -m render data$modelName/modelDetails.json data$modelName/modesPostG.yaml -o data$modelName/modes.html -s 200 --vert 3 &&
+    python -m render data$modelName/modelDetails.json data$modelName/dispsGrav.yaml -o data$modelName/dispsGrav.html -s 200 --vert 3 &&
     # python ../../Scripts/ModeID.py 
     python ../../Scripts/postprocessing/plotPds.py data$modelName/ data$modelName/;
     # python ../../Scripts/plotRH.py senorsRH.out datahwd10.1.$i/RH.out &&
     # python ../../Scripts/compareRH.py sensorRH.out datahwd10.1.$i/RH.out &&
     # python ../../Scripts/png2md.py datahwd10.1.$i/plot.png &&
-    done 
+done
